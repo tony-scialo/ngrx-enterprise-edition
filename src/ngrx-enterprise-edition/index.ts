1822d9cbfed3852c23a24ab8575becd63d7bcd7c
Scialo, Tony <tony_scialo@csx.com>
2019-01-29 14:16:30 -0500

    added simple code to index.ts to start generation of template files

--- src/ngrx-enterprise-edition/index.ts
+++ src/ngrx-enterprise-edition/index.ts
@@ -1,10 +1,27 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { Options } from './schema';
+import {
+  Rule,
+  SchematicContext,
+  Tree,
+  apply,
+  url,
+  template,
+  chain,
+  branchAndMerge,
+  mergeWith
+} from '@angular-devkit/schematics';
+import { strings } from '@angular-devkit/core';
 
-
-// You don't have to export the function as default. You can also have more than one rule factory
-// per file.
-export function ngrxEnterpriseEdition(_options: any): Rule {
+export function ngrxEnterpriseEdition(_options: Options): Rule {
   return (tree: Tree, _context: SchematicContext) => {
-    return tree;
+    const templateSource = apply(url('./files'), [
+      template({
+        ...strings,
+        ..._options
+      })
+    ]);
+    const rule = chain([branchAndMerge(chain([mergeWith(templateSource)]))]);
+
+    return rule(tree, _context);
   };
 }
