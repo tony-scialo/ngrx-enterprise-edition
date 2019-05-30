e5700cd42752f994f91035b0acc44d2f57cbfa30
Scialo, Tony <tony_scialo@csx.com>
2019-05-30 13:07:46 -0400

    updated index.ts to use correct string transformation (dasherize)

--- src/ngrx-enterprise-edition/ngrx-generate-store/ngrx-store-templates/__name@dasherize__-store/index.ts
+++ src/ngrx-enterprise-edition/ngrx-generate-store/ngrx-store-templates/__name@dasherize__-store/index.ts
@@ -2,6 +2,6 @@ import * as <%= classify(name) %>Actions from './actions';
 import * as <%= classify(name) %>Selectors from './selectors';
 import * as <%= classify(name) %>State from './state';
 
-export { <%= classify(name) %>StoreModule } from './<%= decamelize(name) %>-store.module';
+export { <%= classify(name) %>StoreModule } from './<%= dasherize(name) %>-store.module';
 
 export { <%= classify(name) %>Actions, <%= classify(name) %>Selectors, <%= classify(name) %>State };
